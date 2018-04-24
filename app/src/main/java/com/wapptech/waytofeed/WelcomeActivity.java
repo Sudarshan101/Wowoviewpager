8093f1ee8196108f9d502d3f5ecbdadeae6c5a40
Sudarshan101 <sudarshan.vis101@gmail.com.com>
2018-04-24 11:50:54 +0530

    new changes

---
+++ app/src/main/java/com/wapptech/waytofeed/WelcomeActivity.java
@@ -0,0 +1,119 @@
+package com.wapptech.waytofeed;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Handler;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
+
+public class WelcomeActivity extends AppCompatActivity {
+    private static final String TAG = WelcomeActivity.class.getSimpleName();
+    private static int SPLASH_TIME_OUT = 5000;
+    private CoordinatorLayout coordinatorLayout;
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_welcome);
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.welcomelayout);
+        StartAnimations();
+//        gotopage();
+
+    }
+
+    private void StartAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        LinearLayout l=(LinearLayout) findViewById(R.id.lin_lay);
+        l.clearAnimation();
+        l.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.logo);
+        TextView tx = (TextView) findViewById(R.id.fullscreen_content);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+        tx.clearAnimation();
+        tx.startAnimation(anim);
+        gotopage();
+    }
+
+    private void gotopage() {
+        if(isOnline()){
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(getApplicationContext(), IntroActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+        }else{
+            Snackbar snackbar = Snackbar.make(coordinatorLayout, "No internet connection!", Snackbar.LENGTH_LONG)
+                    .setAction("RETRY", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            gotopage();
+                        }
+                    });
+            // Changing message text color
+            snackbar.setActionTextColor(Color.RED);
+            // Changing action button text color
+            View sbView = snackbar.getView();
+            TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
+            textView.setTextColor(Color.YELLOW);
+            snackbar.setDuration((int) TimeUnit.MINUTES.toMillis(1));
+            snackbar.show();
+        }
+    }
+
+    protected boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+    }
+
+}
---
+++ app/src/main/java/com/wapptech/waytofeed/WoWoUtil.java
@@ -0,0 +1,65 @@
+package com.wapptech.waytofeed;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Point;
+import android.util.DisplayMetrics;
+import android.view.Display;
+
+
+class WoWoUtil {
+
+    private static int screenWidth = -1;
+    private static int screenHeight = -1;
+
+    /**
+     * get the screen width in pixels
+     * @param context
+     * @return
+     */
+    public static int getScreenWidth(Context context) {
+        if (screenWidth == -1) {
+            Display display = ((Activity)context).getWindowManager().getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+            screenWidth = size.x;
+        }
+        return screenWidth;
+    }
+
+    /**
+     * get the screen height in pixels
+     * @param context
+     * @return
+     */
+    public static int getScreenHeight(Context context) {
+        if (screenHeight == -1) {
+            Display display = ((Activity)context).getWindowManager().getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+            screenHeight = size.y;
+        }
+        return screenHeight;
+    }
+
+    /**
+     * dp to px
+     * @param dp
+     * @param mContext
+     * @return
+     */
+    public static int dp2px(int dp, Context mContext) {
+        DisplayMetrics displayMetrics = mContext.getResources().getDisplayMetrics();
+        int px = Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+        return px;
+    }
+
+    private static WoWoUtil ourInstance = new WoWoUtil();
+
+    public static WoWoUtil getInstance() {
+        return ourInstance;
+    }
+
+    private WoWoUtil() {
+    }
+}
